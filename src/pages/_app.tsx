a8c357a702b1771f792e44cf370b4998ab7cb3a3
Darksied95 <aliyuefe9501@gmail.com>
2023-06-21 16:46:15 +0100

    added static files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
+import Head from "next/head"
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return<>
+  <Head>
+  <title>LeetClone</title>
+  <link rel='icon' href='/favicon.png'/>
+  <meta name='viewport' content='width=device-width, initial-scale=1'/>
+  <meta name='description' content='web application that contains leetcod problem and  video solution'/>
+  </Head>
+  <Component {...pageProps} />
+  </> 
 }
