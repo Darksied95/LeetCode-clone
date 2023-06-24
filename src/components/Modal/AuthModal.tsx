a20a4b8b2d13c372bba53e45854878a2ec71f67b
Darksied95 <aliyuefe9501@gmail.com>
2023-06-24 22:02:30 +0100

    added switches between login/signup/register

--- src/components/Modal/AuthModal.tsx
+++ src/components/Modal/AuthModal.tsx
@@ -3,13 +3,15 @@ import {IoClose} from 'react-icons/io5'
 import Login from './Login';
 import Signup from './Signup';
 import ResetPassword from './ResetPassword';
+import {  useRecoilValue } from 'recoil';
+import { authModalState } from '@/atoms/authModalAtom';
 
 type AuthModalProps = {
     
 };
 
 const AuthModal:React.FC<AuthModalProps> = () => {
-    
+    const authModal = useRecoilValue(authModalState)
     return <>
     <div
         className='absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60'
@@ -25,9 +27,7 @@ const AuthModal:React.FC<AuthModalProps> = () => {
                         <IoClose className='h-5 w-5' />
                     </button>
                 </div>
-                    {/* <Login /> */}
-                    {/* <Signup /> */}
-                    <ResetPassword />
+                    {authModal.type === 'login' ? <Login /> : authModal.type === 'signup' ? <Signup /> : <ResetPassword />  }
             </div>
         </div>
     </div>
