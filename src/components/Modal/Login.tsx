b5fb27ae97e7f85ebe3b93ddffff9752484f601f
Darksied95 <aliyuefe9501@gmail.com>
2023-07-02 13:17:08 +0100

    added routing back to homepage if user already exists

--- src/components/Modal/Login.tsx
+++ src/components/Modal/Login.tsx
@@ -37,7 +37,7 @@ const Login:React.FC<LoginProps> = () => {
         }
     }
 
-useEffect(() => {error && alert(error) }, [error]);
+    useEffect(() => {error && alert(error) }, [error]);
 
     return <form className='space-y-6 px-6 pb-4' onSubmit={handleSubmit}>
     <h3 className='text-xl font-medium text-white'>Sign in to LeetClone</h3>
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,15 +1,28 @@
 import AuthModal from '@/components/Modal/AuthModal';
 import Navbar from '@/components/Navbar/Navbar';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {useRecoilValue} from 'recoil'
 import { authModalState } from '@/atoms/authModalAtom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/firebase';
+import { useRouter } from 'next/router';
 
 
 type AuthPageProps = {};
 
 const AuthPage:React.FC<AuthPageProps> = () => {
     const authModal = useRecoilValue(authModalState)
+    const [user, loading, error] = useAuthState(auth)
+    const [pageLoading, setpageLoading] = useState(true);
+    const router = useRouter()
+
+    useEffect(() => {
+        if(user) router.push('/')
+        if(!user && !loading) setpageLoading(false)
+    }, [loading, router, user]);
+    
+    if(pageLoading) return null
     
     return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
     <div className='max-w-7xl mx-auto'>
