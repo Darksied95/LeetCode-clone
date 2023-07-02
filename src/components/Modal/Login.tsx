50003d52d903f5912f8897fa73f4779931b3865b
Darksied95 <aliyuefe9501@gmail.com>
2023-07-02 12:41:13 +0100

    added controlled state for login inputs

--- src/components/Modal/Login.tsx
+++ src/components/Modal/Login.tsx
@@ -1,17 +1,43 @@
 import { authModalState } from '@/atoms/authModalAtom';
-import React from 'react';
+import { auth } from '@/firebase/firebase';
+import {  useRouter } from 'next/router';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useSetRecoilState } from 'recoil';
 
-type LoginProps = {
-    
-};
+type LoginProps = {};
 
 const Login:React.FC<LoginProps> = () => {
 
     const setAuthModalState = useSetRecoilState(authModalState)
     const handleClick = (value : 'login' | 'signup' | 'forgotPassword') => {setAuthModalState(prev => ({...prev, type:value}))}
+    const [inputs, setInputs] = useState({email: '', password: ''});
+    const router = useRouter()
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
     
-    return <form className='space-y-6 px-6 pb-4' >
+    
+    function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+        setInputs(prev => ({...prev , [e.target.name] : e.target.value }))
+    }
+
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault()
+        if(!inputs.email || !inputs.password) return alert('Please fill in all fields')
+        try {
+            const  user = await signInWithEmailAndPassword(inputs.email, inputs.password)
+            if(!user ) return
+            router.push('/')
+        } catch (error : any) {
+            alert(error.message)
+        }
+    }
+
+    return <form className='space-y-6 px-6 pb-4' onSubmit={handleSubmit}>
     <h3 className='text-xl font-medium text-white'>Sign in to LeetClone</h3>
     <div>
         <label htmlFor='email' className='text-sm font-medium block mb-2 text-gray-300'>
@@ -21,10 +47,10 @@ const Login:React.FC<LoginProps> = () => {
             type='email'
             name='email'
             id='email'
-            className='
-    border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
-    bg-gray-600 border-gray-500 placeholder-gray-400 text-white
-'
+            value={inputs.email}
+            onChange={handleInput}
+            className='border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
+            bg-gray-600 border-gray-500 placeholder-gray-400 text-white'
             placeholder='name@company.com'
         />
     </div>
@@ -36,19 +62,19 @@ const Login:React.FC<LoginProps> = () => {
             type='password'
             name='password'
             id='password'
-            className='
-    border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
-    bg-gray-600 border-gray-500 placeholder-gray-400 text-white
-'
+            value={inputs.password}
+            onChange={handleInput}
             placeholder='*******'
+            className=' border-2 outline-none sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 
+            bg-gray-600 border-gray-500 placeholder-gray-400 text-white'
         />
     </div>
 
     <button
         type='submit'
         className='w-full text-white focus:ring-blue-300 font-medium rounded-lg
-        text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s
-    '
+        text-sm px-5 py-2.5 text-center bg-brand-orange hover:bg-brand-orange-s'
+        
     >
         Log In
     </button>
