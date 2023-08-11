2842069eb658f5b2999c1b4a46e486615f50628c
Darksied95 <aliyuefe9501@gmail.com>
2023-08-11 22:30:27 +0100

    init editorfooter

---
+++ src/components/Workspace/Playground/EditorFooter.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type EditorFooterProps = {};
+
+const EditorFooter: React.FC<EditorFooterProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default EditorFooter;
