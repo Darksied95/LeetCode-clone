585595f44bbf13b7d25a992477c609edc67cd529
Darksied95 <aliyuefe9501@gmail.com>
2023-08-12 23:58:52 +0100

    add SSG to dynamic problem routes

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,5 +1,6 @@
 import TopBar from "@/components/TopBar/TopBar";
 import Workspace from "@/components/Workspace/Workspace";
+import { problems } from "@/utils/problems";
 import React from "react";
 
 type ProblemPageProps = {};
@@ -13,3 +14,31 @@ const ProblemPage: React.FC<ProblemPageProps> = () => {
   );
 };
 export default ProblemPage;
+
+export async function getStaticPaths() {
+  const paths = Object.keys(problems).map((key) => {
+    params: {
+      pid: key;
+    }
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: { params: { pid: string } }) {
+  const { pid } = params;
+  const problem = problems[pid];
+
+  if (!problem) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { problem },
+  };
+}
