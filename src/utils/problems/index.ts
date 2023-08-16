57d4412e7b81dff768ba3a9f9c278883bd14eed7
Darksied95 <aliyuefe9501@gmail.com>
2023-08-16 21:50:40 +0100

    fix route naming issue

--- src/utils/problems/index.ts
+++ src/utils/problems/index.ts
@@ -14,5 +14,5 @@ export const problems: ProblemMap = {
   "reverse-linked-list": reverseLinkedList,
   "jump-game": jumpGame,
   "search-a-2d-matrix": search2DMatrix,
-  "valid-parenthesis": validParentheses,
+  "valid-parentheses": validParentheses,
 };
