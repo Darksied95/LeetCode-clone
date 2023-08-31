33b30142a7663aac3e632891533df1a058e106b3
Darksied95 <aliyuefe9501@gmail.com>
2023-08-31 20:59:26 +0100

    feat: add like problem functionality

--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -1,41 +1,102 @@
 import React, { useEffect, useState } from "react";
-import { AiFillLike, AiFillDislike } from "react-icons/ai";
+import { AiFillLike, AiFillDislike, AiOutlineLoading3Quarters } from "react-icons/ai";
 import { TiStarOutline } from "react-icons/ti";
 import { BsCheck2Circle } from "react-icons/bs";
 import { DbProblem, Problem } from "@/utils/types/problem";
 import { auth, firestore } from "@/firebase/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, runTransaction } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { toast } from "react-toastify";
 
 type ProblemDescriptionProps = {
   problem: Problem;
 };
 
 const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
-  const { currentProblem, loading, problemDifficultyClass } =
-    useGetCurrentProblem(problem.id);
+  const [user] = useAuthState(auth);
 
-  const { liked, disliked, starred, solved, setData } =
-    useGetUsersDataOnProblem(problem.id);
+  const { currentProblem, loading, problemDifficultyClass, setCurrentProblem } = useGetCurrentProblem(problem.id);
+  const { liked, disliked, starred, solved, setData } = useGetUsersDataOnProblem(problem.id);
+  const [updating, setUpdating] = useState(false);
+
+  async function handleLike() {
+    if (!user) {
+      toast.error("Please Login", { position: "top-center", theme: "dark" });
+      return;
+    }
+
+    if (updating) return;
+    setUpdating(true);
+
+    await runTransaction(firestore, async (transaction) => {
+      const userRef = doc(firestore, "users", user.uid);
+      const problemRef = doc(firestore, "problems", problem.id);
+      const userDoc = await transaction.get(userRef);
+      const problemDoc = await transaction.get(problemRef);
+
+      if (userDoc.exists() && problemDoc.exists()) {
+        if (liked) {
+          transaction.update(userRef, {
+            likedProblems: userDoc.data().likedProblems.filter((id: string) => id !== problem.id),
+          });
+
+          transaction.update(problemRef, {
+            likes: problemDoc.data().likes - 1,
+          });
+
+          setCurrentProblem((prev) => (prev ? { ...prev, likes: prev.likes - 1 } : null));
+
+          setData((prev) => ({ ...prev, liked: false }));
+        } else if (disliked) {
+          transaction.update(userRef, {
+            likedProblems: [...userDoc.data().likedProblems, problem.id],
+            dislikedProblems: userDoc.data().likedProblems.filter((id: string) => id !== problem.id),
+          });
+
+          transaction.update(problemRef, {
+            likes: problemDoc.data().likes + 1,
+            dislikes: problemDoc.data().dislikes - 1,
+          });
+
+          setCurrentProblem((prev) =>
+            prev
+              ? {
+                  ...prev,
+                  likes: prev.likes + 1,
+                  dislikes: prev.dislikes - 1,
+                }
+              : null
+          );
+
+          setData((prev) => ({ ...prev, liked: true, disliked: false }));
+        } else {
+          transaction.update(userRef, {
+            likedProblems: [...userDoc.data().likedProblems, problem.id],
+          });
+
+          transaction.update(problemRef, {
+            likes: problemDoc.data().likes + 1,
+          });
+
+          setCurrentProblem((prev) => (prev ? { ...prev, likes: prev.likes + 1 } : null));
+          setData((prev) => ({ ...prev, liked: true }));
+        }
+      }
+    });
+    setUpdating(false);
+  }
+
   return (
     <div className="bg-dark-layer-1">
       <div className="flex h-11 w-full items-center pt-2 bg-dark-layer-2 text-white overflow-x-hidden">
-        <h1
-          className={
-            "bg-dark-layer-1 rounded-t-[5px] px-5 py-[10px] text-xs cursor-pointer"
-          }
-        >
-          Description
-        </h1>
+        <h1 className={"bg-dark-layer-1 rounded-t-[5px] px-5 py-[10px] text-xs cursor-pointer"}>Description</h1>
       </div>
 
       <div className="flex px-0 py-4 h-[calc(100vh-94px)] overflow-y-auto">
         <div className="px-5">
           <div className="w-full">
             <div className="flex space-x-4">
-              <h2 className="flex-1 mr-2 text-lg text-white font-medium">
-                {problem.title}
-              </h2>
+              <h2 className="flex-1 mr-2 text-lg text-white font-medium">{problem.title}</h2>
             </div>
 
             {!loading && currentProblem && (
@@ -48,8 +109,15 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
                 <div className="rounded p-[3px] ml-4 text-lg transition-colors duration-200 text-green-s text-dark-green-s">
                   <BsCheck2Circle />
                 </div>
-                <div className="flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px]  ml-4 text-lg transition-colors duration-200 text-dark-gray-6">
-                  <AiFillLike className={liked ? "text-dark-blue-s" : ""} />
+                <div
+                  className="flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px]  ml-4 text-lg transition-colors duration-200 text-dark-gray-6"
+                  onClick={handleLike}
+                >
+                  {!updating ? (
+                    <AiFillLike className={liked ? "text-dark-blue-s" : ""} />
+                  ) : (
+                    <AiOutlineLoading3Quarters className="animate-spin" />
+                  )}
                   <span className="text-xs">{currentProblem.likes}</span>
                 </div>
                 <div className="flex items-center cursor-pointer hover:bg-dark-fill-3 space-x-1 rounded p-[3px]  ml-4 text-lg transition-colors duration-200 text-green-s text-dark-gray-6">
@@ -62,20 +130,13 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
               </div>
             )}
 
-            <div
-              className="text-white text-sm"
-              dangerouslySetInnerHTML={{ __html: problem.problemStatement }}
-            />
+            <div className="text-white text-sm" dangerouslySetInnerHTML={{ __html: problem.problemStatement }} />
 
             <div className="mt-4">
               {problem.examples.map((example, index) => (
                 <div key={example.id}>
-                  <p className="font-medium text-white ">
-                    Example {index + 1}:{" "}
-                  </p>
-                  {example.img && (
-                    <img src={example.img} alt="/" className="mt-3" />
-                  )}
+                  <p className="font-medium text-white ">Example {index + 1}: </p>
+                  {example.img && <img src={example.img} alt="/" className="mt-3" />}
                   <div className="example-card">
                     <pre>
                       <strong className="text-white">Input: </strong>
@@ -96,10 +157,7 @@ const ProblemDescription: React.FC<ProblemDescriptionProps> = ({ problem }) => {
 
             <div className="my-5">
               <div className="text-white text-sm font-medium">Constraints:</div>
-              <ul
-                className="text-white ml-5 list-disc"
-                dangerouslySetInnerHTML={{ __html: problem.constraints }}
-              />
+              <ul className="text-white ml-5 list-disc" dangerouslySetInnerHTML={{ __html: problem.constraints }} />
             </div>
           </div>
         </div>
@@ -112,17 +170,21 @@ export default ProblemDescription;
 function useGetCurrentProblem(problemId: string) {
   const [currentProblem, setCurrentProblem] = useState<DbProblem | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
-  const [problemDifficultyClass, setProblemDifficultyClass] =
-    useState<string>("");
+  const [problemDifficultyClass, setProblemDifficultyClass] = useState<string>("");
 
   useEffect(() => {
     (async () => {
       setLoading(true);
+
       const docRef = doc(firestore, "problems", problemId);
+
       const docSnap = await getDoc(docRef);
+
       if (docSnap.exists()) {
         const problem = docSnap.data();
+
         setCurrentProblem({ id: docSnap.id, ...problem } as DbProblem);
+
         setProblemDifficultyClass(
           problem.difficulty === "Easy"
             ? "bg-olive text-olive"
@@ -130,35 +192,35 @@ function useGetCurrentProblem(problemId: string) {
             ? "bg-dark-yellow text-dark-yellow"
             : "bg-dark-pink text-dark-pink"
         );
+
         setLoading(false);
       }
     })();
   }, [problemId]);
 
-  return { loading, currentProblem, problemDifficultyClass };
+  return { loading, currentProblem, problemDifficultyClass, setCurrentProblem };
 }
 
 function useGetUsersDataOnProblem(problemId: string) {
+  const [user] = useAuthState(auth);
+
   const [data, setData] = useState({
     liked: false,
     disliked: false,
     starred: false,
     solved: false,
   });
-  const [user] = useAuthState(auth);
 
   useEffect(() => {
     async function getUsersDataOnProblem() {
       const userRef = doc(firestore, "users", user!.uid);
+
       const userSnap = await getDoc(userRef);
+
       if (userSnap.exists()) {
         const data = userSnap.data();
-        const {
-          solvedProblems,
-          likedProblems,
-          dislikedProblems,
-          starredProblems,
-        } = data;
+
+        const { solvedProblems, likedProblems, dislikedProblems, starredProblems } = data;
 
         setData({
           liked: likedProblems.includes(problemId),
