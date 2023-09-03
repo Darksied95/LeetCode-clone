ffe3f43c7c061bcd57c0046487779733aabc395b
Darksied95 <aliyuefe9501@gmail.com>
2023-09-03 20:47:50 +0100

    feat: add switching between problem functionality

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -9,6 +9,8 @@ import Image from "next/image";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { BsList } from "react-icons/bs";
 import Timer from "../Timer/Timer";
+import { useRouter } from "next/router";
+import { problems } from "@/utils/problems";
 
 type TopBarProps = {
   problemPage?: boolean;
@@ -17,39 +19,51 @@ type TopBarProps = {
 const TopBar: React.FC<TopBarProps> = ({ problemPage }) => {
   const [user] = useAuthState(auth);
   const setAuthModal = useSetRecoilState(authModalState);
+  const router = useRouter();
+
+  function handleProblemChange(isForward: boolean) {
+    const { order } = problems[router.query.pid as string];
+    const direction = isForward ? 1 : -1;
+    const nextProblemOrder = order + direction;
+
+    const nextProblemKey = Object.keys(problems).find((key) => problems[key].order === nextProblemOrder);
+
+    if (isForward && !nextProblemKey) {
+      const firstProblemKey = Object.keys(problems).find((key) => problems[key].order === 1);
+      router.push("/problems/" + firstProblemKey);
+    } else if (!isForward && !nextProblemKey) {
+      const lastProblemKey = Object.keys(problems).find((key) => problems[key].order === Object.keys(problems).length);
+      router.push("/problems/" + lastProblemKey);
+    } else {
+      router.push("/problems/" + nextProblemKey);
+    }
+  }
 
   return (
     <nav className="relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7">
-      <div
-        className={`flex w-full items-center justify-between ${
-          !problemPage ? " max-w-[1200px] mx-auto" : ""
-        }`}
-      >
+      <div className={`flex w-full items-center justify-between ${!problemPage ? " max-w-[1200px] mx-auto" : ""}`}>
         <Link href="/" className="h-[22px] flex-1">
-          <Image
-            src="/logo-full.png"
-            alt="Logo"
-            className="h-full"
-            height={100}
-            width={100}
-          />
+          <Image src="/logo-full.png" alt="Logo" className="h-full" height={100} width={100} />
         </Link>
 
         {problemPage && (
           <div className="flex items-center gap-4 flex-1 justify-center">
-            <div className="flex items-center justify-center rounded dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer">
+            <div
+              className="flex items-center justify-center rounded dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer"
+              onClick={() => handleProblemChange(false)}
+            >
               <FaChevronLeft />
             </div>
-            <Link
-              href="/"
-              className="flex items-center gap-2 font-medium max-w-[170px] text-dark-gray-8 cursor-pointer"
-            >
+            <Link href="/" className="flex items-center gap-2 font-medium max-w-[170px] text-dark-gray-8 cursor-pointer">
               <div>
                 <BsList />
               </div>
               <p>Problem List</p>
             </Link>
-            <div className="flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer">
+            <div
+              className="flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer"
+              onClick={() => handleProblemChange(true)}
+            >
               <FaChevronRight />
             </div>
           </div>
@@ -70,9 +84,7 @@ const TopBar: React.FC<TopBarProps> = ({ problemPage }) => {
             <Link href="/auth">
               <button
                 className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded "
-                onClick={() =>
-                  setAuthModal(() => ({ isOpen: true, type: "login" }))
-                }
+                onClick={() => setAuthModal(() => ({ isOpen: true, type: "login" }))}
               >
                 Sign In
               </button>
@@ -81,13 +93,7 @@ const TopBar: React.FC<TopBarProps> = ({ problemPage }) => {
           {user && problemPage && <Timer />}
           {user && (
             <div className="cursor-pointer group relative">
-              <Image
-                src="/avatar.png"
-                alt="user profile img"
-                className="h-8 w-8 rounded-full"
-                height={100}
-                width={100}
-              />
+              <Image src="/avatar.png" alt="user profile img" className="h-8 w-8 rounded-full" height={100} width={100} />
 
               <div
                 className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg z-40 group-hover:scale-100 scale-0 
