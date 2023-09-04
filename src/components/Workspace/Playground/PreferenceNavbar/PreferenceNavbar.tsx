4252dd7956a2509a009a21a2357db4838e9832a8
Darksied95 <aliyuefe9501@gmail.com>
2023-09-05 00:26:34 +0100

    feat: add fullscreen functionality

--- src/components/Workspace/Playground/PreferenceNavbar/PreferenceNavbar.tsx
+++ src/components/Workspace/Playground/PreferenceNavbar/PreferenceNavbar.tsx
@@ -1,17 +1,42 @@
-import React from "react";
-import { AiOutlineSetting, AiOutlineFullscreen } from "react-icons/ai";
+import React, { useEffect, useState } from "react";
+import { AiOutlineSetting, AiOutlineFullscreen, AiOutlineFullscreenExit } from "react-icons/ai";
 
 type PreferenceNavbarProps = {};
 
 const PreferenceNavbar: React.FC<PreferenceNavbarProps> = () => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  function handleFullScreen(): void {
+    if (isFullScreen) {
+      document.exitFullscreen();
+    } else {
+      document.documentElement.requestFullscreen();
+    }
+    setIsFullScreen(!isFullScreen);
+  }
+
+  useEffect(() => {
+    function exitHandler(e: any) {
+      if (!document.fullscreenElement) {
+        setIsFullScreen(false);
+        return;
+      }
+
+      setIsFullScreen(true);
+    }
+    if (document.addEventListener) {
+      document.addEventListener("fullscreenchange", exitHandler);
+      document.addEventListener("webkitfullscreenchange", exitHandler);
+      document.addEventListener("mozfullscreenchange", exitHandler);
+      document.addEventListener("Msfullscreenchange", exitHandler);
+    }
+  }, []);
+
   return (
     <div className="flex items-center justify-between bg-dark-layer-2 h-11 w-full">
       <div className="flex items-center text-white">
         <button className="flex cursor-pointer items-center rounded focus:outline-none bg-dark-fill-3 text-dark-label-2 hover:bg-dark-fill-2  px-2 py-1.5 font-medium">
           <div className="flex items-center px-1">
-            <p className="text-xs text-label-2 dark:text-dark-label-2">
-              JavaScript
-            </p>
+            <p className="text-xs text-label-2 dark:text-dark-label-2">JavaScript</p>
           </div>
         </button>
       </div>
@@ -24,9 +49,9 @@ const PreferenceNavbar: React.FC<PreferenceNavbarProps> = () => {
           <span className="preferenceBtn-tooltip">Settings</span>
         </button>
 
-        <button className="preferenceBtn group">
+        <button className="preferenceBtn group" onClick={handleFullScreen}>
           <div className="h-4 w-4 text-dark-gray-6 font-bold text-lg">
-            <AiOutlineFullscreen />
+            {!isFullScreen ? <AiOutlineFullscreen /> : <AiOutlineFullscreenExit />}
           </div>
           <span className="preferenceBtn-tooltip">Full Screen</span>
         </button>
