69af29740573575eb48f1fdad5796d930f2db37a
Darksied95 <aliyuefe9501@gmail.com>
2023-09-04 21:10:05 +0100

    add confetti to full screen

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -7,56 +7,37 @@ import { javascript } from "@codemirror/lang-javascript";
 import EditorFooter from "./EditorFooter";
 import { Problem } from "@/utils/types/problem";
 
-type PlagroundProps = {
+type PlaygroundProps = {
   problem: Problem;
+  setSuccess: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const Plaground: React.FC<PlagroundProps> = ({ problem }) => {
+const Playground: React.FC<PlaygroundProps> = ({ problem, setSuccess }) => {
   const [activeTestCaseId, setActiveTestCaseId] = useState(0);
   return (
     <div className="flex flex-col bg-dark-layer-1 relative overflow-x-hidden">
       <PreferenceNavbar />
-      <Split
-        className="h-[calc(100vh-94px)]"
-        direction="vertical"
-        sizes={[60, 40]}
-        minSize={60}
-      >
+      <Split className="h-[calc(100vh-94px)]" direction="vertical" sizes={[60, 40]} minSize={60}>
         <div className="w-full overflow-auto">
-          <CodeMirror
-            value={problem.starterCode}
-            theme={vscodeDark}
-            extensions={[javascript()]}
-            style={{ fontSize: 16 }}
-          />
+          <CodeMirror value={problem.starterCode} theme={vscodeDark} extensions={[javascript()]} style={{ fontSize: 16 }} />
         </div>
         <div className="w-full px-5 overflow-auto">
           <div className="flex h-10 items-center space-x-6">
             <div className="relative flex h-full flex-col justify-center cursor-pointer">
-              <div className="text-sm font-medium leading-5 text-white">
-                Testcases
-              </div>
+              <div className="text-sm font-medium leading-5 text-white">Testcases</div>
               <hr className="absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white" />
             </div>
           </div>
 
           <div className="flex gap-3">
             {problem.examples.map((example, index) => (
-              <div
-                className=" items-start mt-2 text-white"
-                key={example.id}
-                onClick={() => setActiveTestCaseId(index)}
-              >
+              <div className=" items-start mt-2 text-white" key={example.id} onClick={() => setActiveTestCaseId(index)}>
                 <div className="flex flex-wrap items-center gap-y-4">
                   <div
                     className={`font-medium items-center transition-all focus:outline-none 
                     inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg 
                     px-4 py-1 cursor-pointer whitespace-nowrap
-                    ${
-                      activeTestCaseId === index
-                        ? "text-white"
-                        : "text-gray-500"
-                    }`}
+                    ${activeTestCaseId === index ? "text-white" : "text-gray-500"}`}
                   >
                     Case {index + 1}
                   </div>
@@ -80,4 +61,4 @@ const Plaground: React.FC<PlagroundProps> = ({ problem }) => {
     </div>
   );
 };
-export default Plaground;
+export default Playground;
