62e6d74cbb12f4358b93f19ba8be491eaa52c975
Darksied95 <aliyuefe9501@gmail.com>
2023-09-06 19:58:12 +0100

    fix:  show tick for solved problems on homepage

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -4,23 +4,24 @@ import { AiFillYoutube } from "react-icons/ai";
 import YouTube from "react-youtube";
 import Link from "next/link";
 import { IoClose } from "react-icons/io5";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
-import { firestore } from "@/firebase/firebase";
+import { collection, doc, getDoc, getDocs, orderBy, query } from "firebase/firestore";
+import { auth, firestore } from "@/firebase/firebase";
 import { DbProblem } from "@/utils/types/problem";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 type ProblemsTableProps = {
   setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const ProblemsTable: React.FC<ProblemsTableProps> = ({
-  setLoadingProblems,
-}) => {
+const ProblemsTable: React.FC<ProblemsTableProps> = ({ setLoadingProblems }) => {
   const [youtubePlayer, setYoutubePlayer] = useState({
     isOpen: false,
     videoId: "",
   });
 
   const problems: DbProblem[] = useGetProblems(setLoadingProblems);
+  const solvedProblems = useGetSolvedProblems();
+  console.log(solvedProblems);
 
   function closeModal() {
     setYoutubePlayer({
@@ -44,18 +45,11 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({
       <tbody className="text-white">
         {problems.map((problem, idx) => {
           const difficultyColor =
-            problem.difficulty === "Easy"
-              ? "text-dark-green-s"
-              : problem.difficulty === "Medium"
-              ? "text-dark-yellow"
-              : "text-dark-pink";
+            problem.difficulty === "Easy" ? "text-dark-green-s" : problem.difficulty === "Medium" ? "text-dark-yellow" : "text-dark-pink";
           return (
-            <tr
-              className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`}
-              key={problem.id}
-            >
+            <tr className={`${idx % 2 == 1 ? "bg-dark-layer-1" : ""}`} key={problem.id}>
               <th className="px-2 py-4 font-medium whitespace-nowrap text-dark-green-s">
-                <BsCheckCircle fontSize={"18"} width="18" />
+                {solvedProblems.includes(problem.id) && <BsCheckCircle fontSize={"18"} width="18" />}
               </th>
               <td className="px-6 py-4">
                 <Link
@@ -66,9 +60,7 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({
                   {problem.title}
                 </Link>
               </td>
-              <td className={`px-6 py-4 ${difficultyColor}`}>
-                {problem.difficulty}
-              </td>
+              <td className={`px-6 py-4 ${difficultyColor}`}>{problem.difficulty}</td>
               <td className="px-6 py-4">{problem.category}</td>
               <td className="px-6 py-4">
                 {problem.videoId ? (
@@ -92,23 +84,12 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({
       </tbody>
       {youtubePlayer.isOpen && (
         <tfoot className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center ">
-          <div
-            className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute"
-            onClick={closeModal}
-          ></div>
+          <div className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute" onClick={closeModal}></div>
           <div className="w-full z-50 h-full px-6 relative max-w-4xl">
             <div className="w-full h-full flex items-center justify-center relative">
               <div className="w-full relative">
-                <IoClose
-                  fontSize={"35"}
-                  className="cursor-pointer absolute -top-16 right-0"
-                  onClick={closeModal}
-                />
-                <YouTube
-                  videoId={youtubePlayer.videoId}
-                  loading="lazy"
-                  iframeClassName="w-full min-h-[500px]"
-                />
+                <IoClose fontSize={"35"} className="cursor-pointer absolute -top-16 right-0" onClick={closeModal} />
+                <YouTube videoId={youtubePlayer.videoId} loading="lazy" iframeClassName="w-full min-h-[500px]" />
               </div>
             </div>
           </div>
@@ -119,17 +100,12 @@ const ProblemsTable: React.FC<ProblemsTableProps> = ({
 };
 export default ProblemsTable;
 
-function useGetProblems(
-  setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>
-) {
+function useGetProblems(setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>) {
   const [problems, setProblems] = useState<DbProblem[]>([]);
 
   useEffect(() => {
     (async () => {
-      const q = query(
-        collection(firestore, "problems"),
-        orderBy("order", "asc")
-      );
+      const q = query(collection(firestore, "problems"), orderBy("order", "asc"));
 
       const querySnapshot = await getDocs(q);
       const tempArr: DbProblem[] = [];
@@ -141,4 +117,27 @@ function useGetProblems(
     })();
   }, [setLoadingProblems]);
   return problems;
+}
+
+function useGetSolvedProblems() {
+  const [solvedProblems, setSolvedProblems] = useState<string[]>([]);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    const getSolvedProblems = async () => {
+      const userRef = doc(firestore, "users", user!.uid);
+      const userDoc = await getDoc(userRef);
+
+      if (userDoc.exists()) {
+        setSolvedProblems(userDoc.data().solvedProblems);
+      }
+    };
+
+    if (user) {
+      getSolvedProblems();
+    } else {
+      setSolvedProblems([]);
+    }
+  }, [solvedProblems, user]);
+  return solvedProblems;
 }
